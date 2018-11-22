0581b89ec48f0657ffd476f338f416cabe24f384
animatinator <animatinator@gmail.com>
2018-11-22 00:17:37 +0000

    More arithmetic problems

--- src/main/scala/com/animatinator/scala/learn/ninetynineproblems/ArithmeticProblems.scala
+++ src/main/scala/com/animatinator/scala/learn/ninetynineproblems/ArithmeticProblems.scala
@@ -4,6 +4,26 @@ class S99Int(val start: Int) {
   import S99Int._
 
   def isPrime : Boolean = start > 1 && (primes takeWhile {_ <= Math.sqrt(start)} forall {start % _ != 0})
+
+  def isCoPrimeTo(other : Int) : Boolean = ArithmeticProblems.gcd(start, other) == 1
+
+  def totient: Int = (1 to start) count { x => x isCoPrimeTo start}
+
+  def primeFactors: List[Int] = {
+    def primeFactorsInner(rem : Int, primes : Stream[Int]) : List[Int] = {
+      if (rem.isPrime) List(rem)
+      else if (rem % primes.head == 0) primes.head :: primeFactorsInner(rem / primes.head, primes)
+      else primeFactorsInner(rem, primes.tail)
+    }
+
+    primeFactorsInner(start, primes)
+  }
+
+  def primeFactorsMultiplicity : List[(Int, Int)] = ListProblems.encode(primeFactors) map {_.swap}
+
+  def phi : Int = start.primeFactorsMultiplicity.foldLeft(1) {
+    (r, f) => f match {case (factor, mult) => r * (factor - 1) * Math.pow(factor, mult- 1).toInt}
+  }
 }
 
 object S99Int {
@@ -12,6 +32,12 @@ object S99Int {
   val primes = Stream.cons(2, Stream.from(3, 2) filter { _.isPrime })
 }
 
-class ArithmeticProblems extends App {
-  S99Int.primes take 20
+object ArithmeticProblems {
+  def gcd(x : Int, y : Int) : Int = x % y match {
+    case 0 => y
+    case n => gcd(y, n)
+  }
+
+  def listPrimesInRange(range : Range) : List[Int] =
+    S99Int.primes dropWhile(_ < range.start) takeWhile(_ <= range.end) toList
 }
--- src/test/scala/com/animatinator/scala/learn/ninetynineproblems/ArithmeticProblemsTest.scala
+++ src/test/scala/com/animatinator/scala/learn/ninetynineproblems/ArithmeticProblemsTest.scala
@@ -17,4 +17,80 @@ class ArithmeticProblemsTest extends FunSuite {
   test("isPrime_clearlyPrime") {
     assert(7.isPrime)
   }
+
+  test("gcd_equal") {
+    assert(ArithmeticProblems.gcd(7, 7) == 7)
+  }
+
+  test("gcd_greatest") {
+    assert(ArithmeticProblems.gcd(8, 4) == 4)
+  }
+
+  test("gcd_trickier") {
+    assert(ArithmeticProblems.gcd(36, 40) == 4)
+  }
+
+  test("gcd_coprime") {
+    assert(ArithmeticProblems.gcd(37, 40) == 1)
+  }
+
+  test("gcd_secondMultipleOfFirst") {
+    assert(ArithmeticProblems.gcd(4, 8) == 4)
+  }
+
+  test("isCoprimeTo_notCoprime") {
+    assert(!8.isCoPrimeTo(4))
+  }
+
+  test("isCoprimeTo_coprime") {
+    assert(9.isCoPrimeTo(4))
+  }
+
+  test("totient_example") {
+    assert(10.totient == 4)
+  }
+
+  test("totient_prime") {
+    assert(11.totient == 10)
+  }
+
+  test("primeFactors_none") {
+    assert(7.primeFactors == List(7))
+  }
+
+  test("primeFactors_315") {
+    assert(315.primeFactors == List(3, 3, 5, 7))
+  }
+
+  test("primeFactorMultiplicity_315") {
+    assert(315.primeFactorsMultiplicity == List((3, 2), (5, 1), (7, 1)))
+  }
+
+  test("phi_example") {
+    assert(10.phi == 4)
+  }
+
+  test("phi_prime") {
+    assert(11.phi == 10)
+  }
+
+  def time[A](label: String)(block: => A): A = {
+    val now = System.currentTimeMillis()
+    val ret = block
+    println(label + ": " + (System.currentTimeMillis() - now) + " ms.")
+    ret
+  }
+
+  test("totient_comparison") {
+    time("totient(1090)") {
+      println(10900.totient)
+    }
+    time("phi(1090)") {
+      println(10900.phi)
+    }
+  }
+
+  test("listPrimesInRange") {
+    assert(ArithmeticProblems.listPrimesInRange(7 to 31) == List(7, 11, 13, 17, 19, 23, 29, 31))
+  }
 }
